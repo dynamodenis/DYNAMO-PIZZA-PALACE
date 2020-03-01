74eb41803a9241e54f7f2419496938bbc87286d2
dynamodens <dmbugua66@gmail.com>
2020-03-01 14:18:43 +0300

    used constructor to retrieve data and appnd it to the order table

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ var totalOrder=0;
 
 // add an order prototype.
 
+Order.prototype.addOrder=function(){
+    return this.pizzaType +","+ this.pizzaSize +","+ this.pizzaCrust +"," + this.pizzaToppings + "," + this.total;
+}
 
 // USER INTERFACE
 // FRONT END
@@ -24,15 +27,11 @@ $(document).ready(function(){
     $('.confirmOrder').click(function(event){
         event.preventDefault(); 
 
- // // This recieves the data value of the type of pizza;
+     // This recieves the data value of the type of pizza;
       var pizza=$('.pizzaType option:selected').val();
       var size=parseInt($('.pizzaSize option:selected').val());
       var toppings=parseInt($('.pizzaToppings option:selected').val());
       var crust=parseInt($('.pizzaCrust option:selected').val());
-      
-      total= size+toppings+crust;
-      console.log(total);
-      console.log(pizza);
 
        // after confirming order the values will hold no data untill you input
        $('.pizzaType').val("");
@@ -40,13 +39,13 @@ $(document).ready(function(){
        $(".pizzaToppings").val("");
        $('.pizzaCrust').val("");  
         
-    //   Validating the pizza form
+     //   Validating the pizza form
         if(!pizza || !size || !toppings || !crust){
             alert("Failed1! Please choose a pizza.")
         }else{
         }
     
-    //   append the total amount to the div
+     //   append the total amount to the div
         $('.pName').append(pizza);
         $('.pSize').append(size);
         $('.pToppings').append(toppings);
@@ -63,13 +62,20 @@ $(document).ready(function(){
         var orderNumber=0;
         orderNumber= +1;
         total= size+toppings+crust;
+
+        // Use constructor to retrieve  pizza order
+        // Use the constructor to append the order on a table
+
+        var order= new Order(pizza,size,toppings,crust,total);
+
+
         // Append the new order to the table order
         $('.orderNumber').append(orderNumber);
-        $('.orderName').append(pizza);
-        $('.orderSize').append(size);
-        $('.orderToppings').append(toppings);
-        $('.orderCrust').append(crust);
-        $('.orderTotal').html(total);
+        $('.orderName').append(order.pizzaType);
+        $('.orderSize').append(order.pizzaSize);
+        $('.orderToppings').append(order.pizzaToppings);
+        $('.orderCrust').append(order.pizzaCrust);
+        $('.orderTotal').append(order.total);
     });
 
     // Home delivery option
